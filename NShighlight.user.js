4c08fff88d60eb46e90487895f86fb6b2526cee5
jmikk <buldog.buldog26@gmail.com>
2022-07-20 12:08:21 -0500

    Create NShighlight.user.js

---
+++ NShighlight.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Auction color
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Colors ask and bis columns on the auctions page
+// @author       9003
+// @match        https://www.nationstates.net/*/page=deck/show_trades=auctions
+// @match        https://www.nationstates.net/page=deck/show_trades=auctions
+// @match        https://www.nationstates.net/*/page=deck/show_market=auctions
+// @match        https://www.nationstates.net/page=deck/show_market=auctions
+// @match        https://www.nationstates.net/page=deck/show_market=auctions?start=*
+// @match        https://www.nationstates.net/*/page=deck/show_trades=bids
+// @match        https://www.nationstates.net/page=deck/show_trades=bids
+// @match        https://www.nationstates.net/*/page=deck/show_trades=bids
+// @match        https://www.nationstates.net/page=deck/show_trades=bids
+// @match        https://www.nationstates.net/page=deck/show_trades=bids?start=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nationstates.net
+// @grant        none
+// ==/UserScript==
+
+(function main() {
+    'use strict';
+if (window.location.href.endsWith("auctions")){
+
+    const sell = document.getElementsByClassName("cardprice_sell");
+    const buy = document.getElementsByClassName("cardprice_buy");
+
+    for (let i = 0; i < sell.length; i++) {
+        let color = "yellow";
+        if (sell[i].textContent == buy[i].textContent) {
+            color = "lightgreen";
+        }
+
+        sell[i].style.background = color;
+        buy[i].parentElement.style.background = color;
+    }}
+
+
+if (window.location.href.includes("show_trades=bids")){
+    var star = document.getElementsByClassName("cardprice_buy")
+    for( let x = 0; x < star.length; x++){
+        if(star[x].textContent.includes("*")) {
+        star[x].parentElement.style.background = "yellow";
+       }
+    }
+}
+
+if (window.location.href.includes("asks")){
+
+    var star2 = document.getElementsByClassName("cardprice_buy")
+    for( let x = 0; x < star2.length; x++){
+        if(star2[x].textContent.includes("*")) {
+        star2[x].parentElement.style.background = "yellow";
+       }
+    }
+
+}
+})();
